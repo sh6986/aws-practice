ddefdf7fdb70fb9c8d0ac25d3d5cef6a04b48430
sh6986 <okok960411@gmail.com>
2022-02-13 22:56:11 +0900

    lecture

--- lecture/routes/post.js
+++ lecture/routes/post.js
@@ -2,63 +2,68 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const AWS = require('aws-sdk');
+const multerS3 = require('multer-s3');
 
-const {Post, Hashtag} = require('../models');
-const {isLoggedIn} = require('./middlewares');
+const { Post, Hashtag } = require('../models');
+const { isLoggedIn } = require('./middlewares');
 
 const router = express.Router();
 
 try {
-    fs.readdirSync('uploads');
+  fs.readdirSync('uploads');
 } catch (error) {
-    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
-    fs.mkdirSync('uploads');
+  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+  fs.mkdirSync('uploads');
 }
 
-const upload = multer({ // body-parser로는 multipart/form-data를 해석 불가하므로 multer 패키지 필요
-    storage: multer.diskStorage({
-        destination(req, file, cb) {    // 저장경로
-            cb(null, 'uploads/');   // 첫번째 파라미터는 에러
-        },
-        filename(req, file, cb) {   // 저장할 파일명(파일명 + 날짜 + 확장자 형식)
-            const ext = path.extname(file.originalname);
-            cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
-        },
-    }),
-    limits: {fileSize: 5 * 1024 * 1024},
+AWS.config.update({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: 'ap-northeast-2',
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: new AWS.S3(),
+    bucket: 'nodebird6986',
+    key(req, file, cb) {
+      cb(null, `original/${Date.now()}${path.basename(file.originalname)}`);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
 });
 
 router.post('/img', isLoggedIn, upload.single('img'), (req, res) => {
-    console.log(req.file);
-    res.json({url: `/img/${req.file.filename}`});   // 파일주소
+  console.log(req.file);
+  res.json({ url: `/img/${req.file.filename}` });
 });
 
-router.post('/', isLoggedIn, upload.none(), async (req, res, next) => {
-    try {
-        const post = await Post.create({
-            content: req.body.content,
-            img: req.body.url,
-            UserId: req.user.id,
-        });
-
-        const hashtags = req.body.content.match(/#[^\s#]*/g);
-
-        if (hashtags) {
-            const result = await Promise.all(
-                hashtags.map(tag => {
-                    return Hashtag.findOrCreate({   // db에 해당 해시태그가 존재하지 않으면 생성
-                        where: {title: tag.slice(1).toLowerCase()},
-                    })
-                }),
-            );
-            console.log(result);
-            await post.addHashtags(result.map(r => r[0]));
-        }
-        res.redirect('/');
-    } catch (error) {
-        console.error(error);
-        next(error);
+const upload2 = multer();
+router.post('/', isLoggedIn, upload2.none(), async (req, res, next) => {
+  try {
+    console.log(req.user);
+    const post = await Post.create({
+      content: req.body.content,
+      img: req.body.url,
+      UserId: req.user.id,
+    });
+    const hashtags = req.body.content.match(/#[^\s#]*/g);
+    if (hashtags) {
+      const result = await Promise.all(
+        hashtags.map(tag => {
+          return Hashtag.findOrCreate({
+            where: { title: tag.slice(1).toLowerCase() },
+          })
+        }),
+      );
+      await post.addHashtags(result.map(r => r[0]));
     }
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 module.exports = router;
